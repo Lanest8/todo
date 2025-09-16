b51f07d805b4f2881eb2b1b857499c0ea3a7eb5c
Lanest <lanest.lin@oocl.com>
2025-09-16 10:50:47 +0800

    feat: get /todos with no todo

---
+++ src/main/java/org/example/todo/TodoController.java
@@ -0,0 +1,17 @@
+package org.example.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @GetMapping
+    public List<Todo> index() {
+        return List.of();
+    }
+}
